9c6625fcb93fc4e512f8a459cf059fec0449e281
ShihabMVN <shihab.ali@moveon.global>
2024-11-18 07:30:46 +0600

    DB seed & factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Tweet;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,49 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Shihab',
+            'email' => 'shihab@gmail.com',
+            'username' => 'shihab',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(99)
+            ->sequence(fn ($sequence) => ['name' => 'Person '.$sequence->index + 2])
+            ->create();
+
+        foreach (range(1, 20) as $user_id) {
+            Tweet::factory()->create(['user_id' => $user_id]);
+            foreach (range(1, 20) as $user_id2) {
+                User::find($user_id)->follows()->attach(User::find($user_id2));
+            }
+        }
+
+        foreach (range(21, 40) as $user_id) {
+            Tweet::factory()->create(['user_id' => $user_id]);
+            foreach (range(21, 40) as $user_id2) {
+                User::find($user_id)->follows()->attach(User::find($user_id2));
+            }
+        }
+
+        foreach (range(41, 60) as $user_id) {
+            Tweet::factory()->create(['user_id' => $user_id]);
+            foreach (range(41, 60) as $user_id2) {
+                User::find($user_id)->follows()->attach(User::find($user_id2));
+            }
+        }
+
+        foreach (range(61, 80) as $user_id) {
+            Tweet::factory()->create(['user_id' => $user_id]);
+            foreach (range(61, 80) as $user_id2) {
+                User::find($user_id)->follows()->attach(User::find($user_id2));
+            }
+        }
+
+        foreach (range(81, 100) as $user_id) {
+            Tweet::factory()->create(['user_id' => $user_id]);
+            foreach (range(81, 100) as $user_id2) {
+                User::find($user_id)->follows()->attach(User::find($user_id2));
+            }
+        }
     }
 }
